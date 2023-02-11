e9758daabb6a0feff8a8ee8bebd3b9ea3cc04862
luluco92 <117111188+luluco92@users.noreply.github.com>
2023-02-10 20:57:20 -0500

    remote add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './components/Card';
+import PortfolioContainer from "./components/PortfolioContainer";
+
+function App() {
+  return (
+    <div>
+   <PortfolioContainer />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
